b91717b587727cc586acdd3a84b0ca2baff2b14f
beatrizrodc <pop.brc.translate@gmail.com>
2024-07-16 12:47:59 -0300

    Java RESTful API criada para o Bootcamp Santander

---
+++ src/main/java/project/dio/ProjectDioTrilhaJavaApplication.java
@@ -0,0 +1,13 @@
+package project.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectDioTrilhaJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectDioTrilhaJavaApplication.class, args);
+	}
+
+}
